20b61ab8d704d03d890ff69ce373a5975d55b7eb
lakshman <lakshmanjoyforever@gmail.com>
2018-11-30 13:41:40 +0530

    Add folder structure

--- WebApp/src/components/app.js
+++ WebApp/src/components/app.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../components/common/authorized/header'
+import Footer from '../components/common/authorized/footer'
 
 class App extends React.Component {
     render() {
         return (
-            <h1>ECart</h1>
+            <div>
+                <Header></Header>
+
+                <Footer></Footer>
+            </div>
         )
     }
 }
--- WebApp/src/index.js
+++ WebApp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import APP from './components/app'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<APP />, document.getElementById("root"));
