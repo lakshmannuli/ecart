876db1fd844a1e7decc44fe19e96d9a046baf759
lakshman <lakshmanjoyforever@gmail.com>
2018-11-30 17:57:26 +0530

    Added mock api

--- WebApp/src/components/common/authorized/header.js
+++ WebApp/src/components/common/authorized/header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Header = (props) => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to="/" className="navbar-brand" >Home</Link>
+            <Link to="/" className="navbar-brand" >E Cart</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
--- WebApp/src/components/products/products.js
+++ WebApp/src/components/products/products.js
@@ -1,10 +1,18 @@
 
 import React from 'react'
+import productsAPI from '../../api/mockProductsAPI'
 
-class Products extends React.Component
-{
-    render(){
-        return(
+class Products extends React.Component {
+    componentDidMount() {
+        productsAPI.getAllProducts().then(response => {
+            console.log(response);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
             <h4>Products</h4>
         )
     }
